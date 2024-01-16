5f7c8cfd1cfad736469010c70b98fc17bc5c14c2
zawkinev <zawkindev@gmail.com>
2024-01-16 18:38:35 +0500

    fix: add bottom padding to App.vue

--- src/App.vue
+++ src/App.vue
@@ -12,7 +12,7 @@ function toggleTheme() {
 <template>
   <div :class="{ dark: isDarkTheme }">
     <div
-      class="flex flex-col pt-20 gap-10 h-full min-h-screen w-screen items-center bg-bgLight dark:bg-bgDark dark:text-white"
+      class="flex flex-col py-20 gap-10 h-full min-h-screen w-screen items-center bg-bgLight dark:bg-bgDark dark:text-white"
     >
       <div class="flex flex-col items-center w-3/6 gap-20 overflow-visible">
         <Navbar @toggle-theme="toggleTheme" :is-dark-theme="isDarkTheme" />
