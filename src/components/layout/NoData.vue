c52bfa79d3634d106ad8ad27ef9ea9fe2fe219ad
zawkindev <zawkindev@gmail.com>
2024-01-22 16:26:52 +0500

    fix: src of no data svg

--- src/components/layout/NoData.vue
+++ src/components/layout/NoData.vue
@@ -1,8 +1,8 @@
 <script setup></script>
 
 <template>
-  <div class="flex flex-col w-96 text-center gap-10">
-    <img alt="empty box" src="../assets/email.svg" />
+  <div class="flex flex-col text-center justify-center gap-10">
+    <img class="flex self-center w-96 m-auto" alt="empty box" src="../../assets/email.svg" />
     <h2 class="font-bold text-3xl">There is nothing here</h2>
     <p class="text-[#888EB0] text-xl">
       Create an invoice by clicking the
